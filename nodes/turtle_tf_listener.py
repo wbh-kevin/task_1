093538f0a56a23e1f94a2aed8c4b3609a4b4944a
wbh-kevin <87693004+wbh-kevin@users.noreply.github.com>
2021-07-21 09:26:27 +0800

    Update turtle_tf_listener.py

--- nodes/turtle_tf_listener.py
+++ nodes/turtle_tf_listener.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     rospy.wait_for_service('spawn')
     spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
     spawner(4, 6, 0, 'turtle2')
-
+#生成turtle2
     turtle_vel = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.Twist, queue_size=1)
     rate = rospy.Rate(10.0)
 
@@ -24,12 +24,13 @@ if __name__ == '__main__':
             (trans,rot) = listener.lookupTransform('/turtle2', '/carrot1',rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
-
+#接受自己相对于carrot1坐标系的位姿
         angular = 10 * math.atan2(trans[1], trans[0])
         linear = 5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
         cmd = geometry_msgs.msg.Twist()
         cmd.linear.x = linear
         cmd.angular.z = angular
         turtle_vel.publish(cmd)
+#设置线速度与角速度
         rate.sleep()
 
