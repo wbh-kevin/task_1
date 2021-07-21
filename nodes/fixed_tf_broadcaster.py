5b7fefc88c16d64e03a859342db4616a0b1ce3f9
wbh-kevin <87693004+wbh-kevin@users.noreply.github.com>
2021-07-21 09:25:32 +0800

    Update fixed_tf_broadcaster.py

--- nodes/fixed_tf_broadcaster.py
+++ nodes/fixed_tf_broadcaster.py
@@ -5,7 +5,7 @@ roslib.load_manifest('learning_tf')
 import rospy
 import tf
 import math
-
+#接受播报并将其转化为播报距离原海龟y轴为2的点，记为carrot1
 if __name__ == '__main__':
     rospy.init_node('my_tf_broadcaster')
     br = tf.TransformBroadcaster()
